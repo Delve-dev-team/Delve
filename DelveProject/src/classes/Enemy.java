8593ec4ef13db3446e975fd2f8c96b23853c5104
xdpikaboo <61671884+xdpikaboo@users.noreply.github.com>
2020-10-22 09:23:41 +0700

    new commit for player and enemy class
    xdxdxdxdxd

--- DelveProject/src/classes/Enemy.java
+++ DelveProject/src/classes/Enemy.java
@@ -2,8 +2,50 @@ package classes;
 
 public class Enemy {
 
-	public Enemy() {
-		
-	}
+	int HP;
+
+    int MP;
+
+    int attackDamage;
+
+    int attackSpeed;
+
+    int movementSpeed;
+
+    int attackRange;
+
+    public Enemy(int level) {
+        HP = 50 * level;
+        MP = 50 * level;
+        attackDamage = 5 * level;
+        attackSpeed = 10;
+        movementSpeed = 10;
+        attackRange = 10;
+    }
+
+    public void attack(Player player) {
+        player.setHP(player.getHP() - (this.attackDamage * this.attackSpeed));
+    }
+
+    public void die(Player player) {
+        this.HP = 0;
+        player.setGold(player.getGold() + 5);
+    }
+
+    public void takeTurn(Map map) {
+
+    }
+
+    public void move(Map map) {
+        
+    }
+
+    public int getHP() {
+        return this.HP;
+    }
+
+    public void setHP(int hp) {
+        this.HP = hp;
+    }
 	
 }
--- DelveProject/src/classes/Player.java
+++ DelveProject/src/classes/Player.java
@@ -1,5 +1,85 @@
 package classes;
 
 public class Player {
+    
+    int HP;
+
+    int MP;
+
+    int attackDamage;
+
+    int attackSpeed;
+
+    int movementSpeed;
+
+    int attackRange;
+
+    int gold;
+
+    boolean headSlot;
+
+    boolean chestSlot;
+
+    boolean armSlot;
+
+    boolean legSlot;
+
+    boolean feetSlot;
+
+    boolean handSlot;
+
+    public Player() {
+        HP = 1000;
+        MP = 1000;
+        attackDamage = 100;
+        attackSpeed = 10;
+        movementSpeed = 10;
+        attackRange = 10;
+        gold = 0;
+        headSlot = false;
+        chestSlot = false;
+        armSlot = false;
+        legSlot = false;
+        feetSlot = false;
+        handSlot = false;
+    }
+
+    // public String EquipItem (Item item) {
+    //     if (this.(getSlot(item)) == false) {
+    //         this.(getSlot(item)) = true;
+    //         return item + " has been equipped.";
+    //     } else {
+    //         return "Sorry, slot " + item + " is full";
+    //     }
+    // }
+
+    // public String UnequipItem (String slot) {
+    //     if (this.slot == true) {
+    //         this.slot = false;
+    //         return item + " has been unequipped.";
+    //     } else {
+    //         return "There was no item in that slot";
+    //     }
+    // }
+
+    public void Attack(Enemy enemy) {
+        enemy.setHP(enemy.getHP() - (this.attackDamage * this.attackSpeed));
+    }
+
+    public int getHP() {
+        return this.HP;
+    }
+
+    public void setHP(int hp) {
+        this.HP = hp;
+    }
+
+    public int getGold() {
+        return this.gold;
+    }
+
+    public void setGold(int gold) {
+        this.gold = gold;
+    }
 
 }
