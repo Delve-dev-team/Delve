3305beb51ae18b7d3a3ac85a4766c95fc759fe19
xdpikaboo <61671884+xdpikaboo@users.noreply.github.com>
2020-10-24 23:57:44 +0700

    Revert "temporary shop"
    This reverts commit fdfb148b56fda957d3ccb20d7a129e9aa186b3c8.

--- DelveProject/src/classes/Shop.java
+++ DelveProject/src/classes/Shop.java
@@ -1,18 +1,11 @@
 package classes;
 
-import java.util.ArrayList;
-
 public class Shop {
 
-	public ArrayList<Item> itemList;
+	public Item[] itemlist;
 
 	public Shop() {
-		itemList.add("Leather Helmet", "headSlot", 10, "+ 100 HP", 0);
-		itemList.add("Leather Armor", "chestSlot", 10, "+ 100 HP", 0);
-		itemList.add("Leather Bracers", "armSlot", 10, "+ 100 HP", 0);
-		itemList.add("Leather Pants", "legSlot", 10, "+ 100 HP", 0);
-		itemList.add("Leather Boots", "feetSlot", 10, "+ 100 HP", 0);
-		itemList.add("Leather Gloves", "handSlot", 10, "+ 100 HP", 0);
+		
 	}
 	
 }
