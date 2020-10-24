f1082e5af480ead3c6d49a41c837016aa80f4637
xdpikaboo <61671884+xdpikaboo@users.noreply.github.com>
2020-10-24 23:59:09 +0700

    temporary shop

--- DelveProject/src/classes/Shop.java
+++ DelveProject/src/classes/Shop.java
@@ -1,11 +1,18 @@
 package classes;
 
+import java.util.ArrayList;
+
 public class Shop {
 
-	public Item[] itemlist;
+	public ArrayList<Item> itemList;
 
 	public Shop() {
-		
+		itemList.add(new Item("Leather Helmet", "headSlot", 10, "+ 100 HP", 0));
+		itemList.add(new Item("Leather Armor", "chestSlot", 10, "+ 100 HP", 0));
+		itemList.add(new Item("Leather Bracers", "armSlot", 10, "+ 100 HP", 0));
+		itemList.add(new Item("Leather Pants", "legSlot", 10, "+ 100 HP", 0));
+		itemList.add(new Item("Leather Boots", "feetSlot", 10, "+ 100 HP", 0));
+		itemList.add(new Item("Leather Gloves", "handSlot", 10, "+ 100 HP", 0));
 	}
 	
 }
