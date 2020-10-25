b8c4d370a65b71fa7a277192414776a0e3d3e6fc
Xiaoge Zhang <gezi12zai@gmail.com>
2020-10-25 14:20:01 -0400

    this commit: --- add testing file in

---
+++ DelveProject/src/testing/GameControllerTest.java
@@ -0,0 +1,48 @@
+package testing;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameControllerTest {
+
+    @Test
+    void main() {
+    }
+
+    @Test
+    void isGameOver() {
+    }
+
+    @Test
+    void isExitReach() {
+    }
+
+    @Test
+    void isShopReach() {
+    }
+
+    @Test
+    void isThereAvailableTarget() {
+    }
+
+    @Test
+    void availableTargets() {
+    }
+
+    @Test
+    void amountOfAttacksFromMonster() {
+    }
+
+    @Test
+    void enterNextLevel() {
+    }
+
+    @Test
+    void getCurrentLevel() {
+    }
+
+    @Test
+    void getMap() {
+    }
+}
